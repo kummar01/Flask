b70a00528fe1e4c2a452a73b0d3658fef066e0ce
Alexander Kumm <kummar01@mail.buffalostate.edu>
2020-06-04 02:59:29 -0400

    First commit

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = 'eaf17543ca648ab9a6ca59dda1cd244f'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
