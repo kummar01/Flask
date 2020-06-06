1067c841f5e3b0120a5d6f0cad24a9716a65508f
Alexander Kumm <kummar01@mail.buffalostate.edu>
2020-06-06 07:53:43 -0400

    first coomit

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -15,3 +15,7 @@ def home():
 @main.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@main.route("/TryAgain")
+def TryAgain():
+    return render_template('TryAgain.html', title='TryAgain')
